86e3a55f5e25baea51d216073d2647c921b0e7ab
Alan Paul Rodriguez Gallardo <147470070+PaulScholl@users.noreply.github.com>
2024-10-27 15:42:52 -0700

    Update 24.pl

--- 24.pl
+++ 24.pl
@@ -36,6 +36,35 @@
 %     }
 % }
 
+% Genera una lista con todos los enteros dentro de un rango dado.
+% Caso base: cuando el inicio y el final son iguales.
+range(I, I, [I]).
+% Caso recursivo: agrega el inicio a la lista y continúa con el siguiente número.
+range(I, K, [I|R]) :- I < K, I1 is I + 1, range(I1, K, R).
+
+% Extrae un número dado de elementos aleatorios de una lista.
+% Caso base: seleccionar 0 elementos resulta en una lista vacía.
+rnd_select(_, 0, []).
+
+% Caso recursivo: selecciona un elemento aleatorio de la lista y llama recursivamente.
+rnd_select(L, N, [X|R]) :- 
+    length(L, Len), 
+    Len > 0,                       % Asegúrate de que la lista no esté vacía.
+    random(1, Len, I), 
+    element_at(X, L, I),           % Obtiene el elemento en la posición I.
+    delete(L, X, L1),              % Elimina el elemento seleccionado de la lista.
+    N1 is N - 1,                   % Decrementa el contador de elementos restantes.
+    rnd_select(L1, N1, R).          % Llama recursivamente con la lista reducida.
+
+% Extrae el elemento en la posición I de la lista L.
+element_at(X, [X|_], 1).                    % Caso base: el primer elemento es el que buscamos.
+element_at(X, [_|T], I) :-                    % Caso recursivo: busca en la cola.
+    I > 1, 
+    I1 is I - 1, 
+    element_at(X, T, I1).
+
 % Extrae N números aleatorios de un rango dado.
 % Utiliza la función range para generar la lista de números y luego selecciona aleatoriamente.
-lotto(N, M, L) :- range(1, M, R), rnd_select(R, N, L).
+lotto(N, M, L) :- 
+    range(1, M, R),        % Genera una lista de números del 1 al M.
+    rnd_select(R, N, L).   % Selecciona N números aleatorios de la lista generada.
