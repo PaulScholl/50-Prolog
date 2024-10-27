6cc43ec6d760b358824341682ac413722f69d7c6
Alan Paul Rodriguez Gallardo <147470070+PaulScholl@users.noreply.github.com>
2024-10-27 14:58:17 -0700

    Update 19.pl
    Agregar función split

--- 19.pl
+++ 19.pl
@@ -36,7 +36,19 @@
 %         lista.AddRange(rotada);
 %     }
 % }
-
 % Rota una lista N lugares a la izquierda.
 % Utiliza la longitud de la lista para manejar rotaciones mayores que la longitud.
-rotate(L, N, R) :- length(L, Len), N1 is N mod Len, split(L, N1, L1, L2), append(L2, L1, R).
+rotate(L, N, R) :- 
+    length(L, Len), 
+    Len > 0,              % Asegura que la lista no esté vacía
+    N1 is N mod Len, 
+    split(L, N1, L1, L2), 
+    append(L2, L1, R).
+
+% Divide una lista en dos partes.
+% La longitud de la primera parte está dada por el segundo argumento.
+split(L, 0, [], L).
+split([H|T], N, [H|L1], L2) :- 
+    N > 0, 
+    N1 is N - 1, 
+    split(T, N1, L1, L2).
