32c82a0cd8fd16a20b5a0c8a4bfb5ad34e60ddb5
Alan Paul Rodriguez Gallardo <147470070+PaulScholl@users.noreply.github.com>
2024-10-28 13:43:52 -0700

    Update 43.pl

--- 43.pl
+++ 43.pl
@@ -1,3 +1,44 @@
+% -------------------------------------------------------------------
+%  Programa: 43.pl
+%  Autor: Alan Paul Rodriguez Gallardo
+%  Fecha: 27 de Octubre de 2024
+%  Objetivo: Código Gray
+%   
+% -------------------------------------------------------------------
+%using System;
+%using System.Collections.Generic;
+%
+%class Program
+%{
+%    static void Main()
+%    {
+%        Console.WriteLine("Ingrese el número de bits para generar el código Gray:");
+%        int n = Convert.ToInt32(Console.ReadLine());
+%
+%        List<string> grayCode = GenerarCodigoGray(n);
+%
+%        Console.WriteLine("Código Gray:");
+%        foreach (var code in grayCode)
+%        {
+%            Console.WriteLine(code);
+%        }
+%    }
+%
+%    static List<string> GenerarCodigoGray(int n)
+%    {
+%        List<string> result = new List<string>();
+%        int totalCodes = 1 << n; // Total de códigos Gray es 2^n
+%
+%        for (int i = 0; i < totalCodes; i++)
+%        {
+%            // Conversión de i a Gray: i ^ (i >> 1)
+%            int gray = i ^ (i >> 1);
+%            result.Add(Convert.ToString(gray, 2).PadLeft(n, '0')); // Convertir a binario y rellenar
+%        }
+%
+%        return result;
+%    }
+%}
 % Genera el código Gray de N bits.
 % https://es.wikipedia.org/wiki/Código_Gray
 % La secuencia de código Gray es una secuencia binaria en la que dos valores consecutivos difieren en solo un bit.
